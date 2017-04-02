4d72cffbcbab07f8541da68f93233152e3249b01
Nuno Carreiro <Nuno Carreiro@DESKTOP-UK3GQC2.lan>
2017-04-02 10:53:46 +0100

    Added vector to string and swap elements of an array

--- Useful/src/UsefulMethods.java
+++ Useful/src/UsefulMethods.java
@@ -4,9 +4,12 @@ public class UsefulMethods {
 
 		int value = 3; // change me
 
+		int v[] = { 1, 2, 3, 4, 5 };
 		System.out.println("Fibonacci " + value + " element:" + fibonacciRecursive(value));
 		System.out.println("Fibonacci " + value + " element:" + fibonacciIter(value));
 		System.out.println("Is " + value + " prime? " + isPrime(value));
+		System.out.println("Array: " + vectorToString(v));
+		System.out.println("Swap array first element with last: " + vectorToString(swap(v, 0, 4)));
 	}
 
 	/**
@@ -37,37 +40,76 @@ public class UsefulMethods {
 	 */
 	public static int fibonacciIter(int n) {
 
-		int res=0;
+		int res = 0;
 		int last = 0;
 		int next = 1;
 		for (int i = 1; i < n; i++) {
-				res = last + next;
-				last = next;
-				next = res;
+			res = last + next;
+			last = next;
+			next = res;
 		}
-		
+
 		return res;
 	}
-	
+
 	/**
 	 * Check if a number is prime in an efficient way
-	 * @param n the number to see if it is prime or not
+	 * 
+	 * @param n
+	 *            the number to see if it is prime or not
 	 * @return true is n is prime false otherwise
 	 */
-	public static boolean isPrime(int n){
-		
-		if(n == 2 )
+	public static boolean isPrime(int n) {
+
+		if (n == 2)
 			return true;
-		if(n%2 ==0)
+		if (n % 2 == 0)
 			return false;
-		
-		for(int i = 3; i<n;i+=2){
-			if(n%i == 0)
+
+		for (int i = 3; i < n; i += 2) {
+			if (n % i == 0)
 				return false;
 		}
 		return true;
-		
+
 	}
-	
-	
+
+	/**
+	 * Swap two elements of an int array without using a temp variable
+	 * 
+	 * @param v
+	 *            the array to swap the elements
+	 * @param f
+	 *            the element at f position to be swapped
+	 * @param l
+	 *            the element at l position to swap with f
+	 * @return the array with the elements f and l swapped
+	 */
+	public static int[] swap(int[] v, int f, int l) {
+
+		v[f] = v[f] + v[l];
+		v[l] = v[f] - v[l];
+		v[f] = v[f] - v[l];
+
+		return v;
+	}
+
+	/**
+	 * Prints an array in a single line
+	 * 
+	 * @param v
+	 *            the array to print
+	 * @return a string with the elements of the array
+	 */
+	public static String vectorToString(int[] v) {
+
+		StringBuilder sb = new StringBuilder("");
+
+		for (int i = 0; i < v.length; i++) {
+			sb.append(v[i] + " ");
+		}
+		sb.deleteCharAt(sb.length() - 1);
+		return sb.toString();
+	}
+
 }
