da86e427517787d3380ee5722b88c5fb99885ea0
Nuno Carreiro <Nuno Carreiro@192.168.1.10>
2018-02-14 16:03:33 +0100

    main implemented

--- Tetris/src/com/nnc/tetris/Board.java
+++ Tetris/src/com/nnc/tetris/Board.java
@@ -1,5 +1,7 @@
 package com.nnc.tetris;
+
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,12 +12,14 @@ import javax.swing.Timer;
 
 import com.nnc.tetris.Shape.Tetronimos;
 
-public class Board extends JPanel implements ActionListener{
+public class Board extends JPanel implements ActionListener {
 
 	private static final int BOARD_WIDTH = 10;
 	private static final int BOARD_HEIGHT = 22;
 	private static final int TIME = 400;
-	private static final Color[] COLORS = {};
+	private static final Color[] COLORS = { new Color(0, 0, 0), new Color(204, 102, 102), new Color(102, 204, 102),
+			new Color(102, 102, 204), new Color(102, 204, 102), new Color(102, 102, 204), new Color(204, 204, 102),
+			new Color(204, 102, 204), new Color(102, 204, 204), new Color(218, 170, 0) };
 	private Timer timer;
 	private boolean isFallingFinished = false;
 	private boolean isStarted = false;
@@ -26,71 +30,110 @@ public class Board extends JPanel implements ActionListener{
 	private JLabel statusBar;
 	private Shape currentPiece;
 	private Tetronimos[] board;
-	
-	public Board(Tetris parent){
+
+	public Board(Tetris parent) {
 		setFocusable(true);
 		currentPiece = new Shape();
 		timer = new Timer(TIME, this);
 		statusBar = parent.getStatusBar();
-		board = new Tetronimos [BOARD_WIDTH * BOARD_HEIGHT];
+		board = new Tetronimos[BOARD_WIDTH * BOARD_HEIGHT];
 		clearBoard();
-		
+
 	}
-	
-	public int squareWidth(){
+
+	public int squareWidth() {
 		return (int) getSize().getWidth() / BOARD_WIDTH;
 	}
-	
-	public int squareWeight(){
+
+	public int squareHeight() {
 		return (int) getSize().getHeight() / BOARD_HEIGHT;
 	}
-	
-	public Tetronimos shapeAt (int x, int y){
+
+	public Tetronimos shapeAt(int x, int y) {
 		return board[y * BOARD_WIDTH + x];
 	}
-	
-	private void clearBoard(){
-		
-		for(int i=0; i< BOARD_HEIGHT * BOARD_WIDTH; i++)
+
+	private void clearBoard() {
+
+		for (int i = 0; i < BOARD_HEIGHT * BOARD_WIDTH; i++)
 			board[i] = Tetronimos.NO_SHAPE;
-		
+
 	}
-	
-	private void pieceDrop(){
+
+	private void pieceDrop() {
 		int x;
 		int y;
-		for(int i=0; i < currentPiece.getX();i++){
+		for (int i = 0; i < currentPiece.getX(); i++) {
 			x = currentX + currentPiece.x(i);
 			y = currentY + currentPiece.y(i);
 			board[y * BOARD_WIDTH + x] = currentPiece.getShape();
 		}
-		
-		if (!isFallingFinished){
+
+		if (!isFallingFinished) {
 			newPiece();
 		}
-		
+
 	}
-	
-	
-	private void newPiece() {
+
+	public void newPiece() {
 		currentPiece.setRandomShape();
 		currentX = BOARD_WIDTH / 2 + 1;
-		currentY = BOARD_HEIGHT -1 + currentPiece.minY(); 
-		
+		currentY = BOARD_HEIGHT - 1 + currentPiece.minY();
+
 	}
-	
-	private void oneLineDown(){
+
+	private void oneLineDown() {
 		pieceDrop();
 	}
 
-	private void drawSquare(Graphics g, int x,int y, Tetronimos shape){
-		
+	private void drawSquare(Graphics g, int x, int y, Tetronimos shape) {
+		Color color = COLORS[shape.ordinal()];
+		g.setColor(color);
+		g.fillRect(x + 1, y + 1, squareWidth() - 2, squareHeight() - 2);
+		g.setColor(color.brighter());
+		g.drawLine(x, y + squareHeight() - 1, x, y);
+		g.drawLine(x, x + squareWidth() - 1, x, y);
+		g.setColor(color.darker());
+		g.drawLine(x + 1, y + squareHeight() - 1, x + squareWidth() - 1, y + squareHeight() - 1);
+		g.drawLine(x + squareWidth() - 1, y + squareHeight() - 1, x + squareWidth() - 1, y + 1);
+
 	}
-	
-	
+
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		Dimension size = getSize();
+
+		int boardTop = (int) size.getHeight() - BOARD_HEIGHT * squareHeight();
+
+		for (int i = 0; i < BOARD_HEIGHT; i++) {
+			for (int j = 0; j < BOARD_WIDTH; j++) {
+				Tetronimos shape = shapeAt(j, BOARD_HEIGHT - i - 1);
+
+				if (shape != Tetronimos.NO_SHAPE) {
+					drawSquare(g, j * squareWidth(), boardTop + i + squareHeight(), shape);
+
+				}
+			}
+
+		}
+
+		if (currentPiece.getShape() != Tetronimos.NO_SHAPE) {
+			int x;
+			int y;
+			for (int i = 0; i < currentPiece.getX(); i++) {
+				x = currentX + currentPiece.x(i);
+				y = currentY + currentPiece.y(i);
+				drawSquare(g, x * squareWidth(), boardTop + (BOARD_HEIGHT - y - 1) * squareHeight(),
+						currentPiece.getShape());
+			}
+		}
+
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		
+
 	}
 
 }
--- Tetris/src/com/nnc/tetris/Tetris.java
+++ Tetris/src/com/nnc/tetris/Tetris.java
@@ -1,5 +1,7 @@
 package com.nnc.tetris;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -7,8 +9,31 @@ public class Tetris extends JFrame{
 	
 	private JLabel statusBar;
 	
+	public Tetris(){
+		//to display lines number
+		statusBar = new JLabel ("0");
+		add(statusBar, BorderLayout.SOUTH);
+		Board board = new Board(this);
+		add(board);
+		
+		//add one piece
+		board.newPiece();
+		board.repaint();
+		
+		setSize(200, 400);
+		setTitle("Tetris");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	
 	public JLabel getStatusBar(){
 		return statusBar;
+	}
+	
+	public void main(String[] args){
+		Tetris tetris = new Tetris();
+		tetris.setLocationRelativeTo(null);
+		tetris.setVisible(true);
 	}
 
 }
