c49a23b132159e487818ab2a5339b318da3bafc3
Nuno Carreiro <Nuno Carreiro@192.168.1.10>
2018-02-14 19:43:25 +0100

    Tetris working - future updates?

--- Tetris/src/com/nnc/tetris/Board.java
+++ Tetris/src/com/nnc/tetris/Board.java
@@ -16,12 +16,12 @@ import com.nnc.tetris.Shape.Tetronimos;
 
 public class Board extends JPanel implements ActionListener {
 
-	private static final int BOARD_WIDTH = 10;
-	private static final int BOARD_HEIGHT = 22;
-	private static final int TIME = 400;
 	private static final Color[] COLORS = { new Color(0, 0, 0), new Color(204, 102, 102), new Color(102, 204, 102),
 			new Color(102, 102, 204), new Color(102, 204, 102), new Color(102, 102, 204), new Color(204, 204, 102),
 			new Color(204, 102, 204), new Color(102, 204, 204), new Color(218, 170, 0) };
+	private static final int BOARD_WIDTH = 10;
+	private static final int BOARD_HEIGHT = 22;
+	private static final int TIME = 400;
 	private Timer timer;
 	private boolean isFallingFinished = false;
 	private boolean isStarted = false;
@@ -34,218 +34,206 @@ public class Board extends JPanel implements ActionListener {
 	private Tetronimos[] board;
 
 	public Board(Tetris parent) {
-		setFocusable(true);
-		currentPiece = new Shape();
-		timer = new Timer(TIME, this);
-		statusBar = parent.getStatusBar();
-		board = new Tetronimos[BOARD_WIDTH * BOARD_HEIGHT];
-		clearBoard();
-		addKeyListener(new TetrisAdapter());
-	}
-
-	public int squareWidth() {
-		return (int) getSize().getWidth() / BOARD_WIDTH;
-	}
-
-	public int squareHeight() {
-		return (int) getSize().getHeight() / BOARD_HEIGHT;
-	}
-
-	public Tetronimos shapeAt(int x, int y) {
-		return board[y * BOARD_WIDTH + x];
-	}
-
-	private void clearBoard() {
-
-		for (int i = 0; i < BOARD_HEIGHT * BOARD_WIDTH; i++)
-			board[i] = Tetronimos.NO_SHAPE;
-
-	}
-
-	private void pieceDrop() {
-		for (int i = 0; i < currentPiece.getX(); i++) {
-			int x = currentX + currentPiece.x(i);
-			int y = currentY + currentPiece.y(i);
-			board[y * BOARD_WIDTH + x] = currentPiece.getShape();
-		}
-		
-		removeFullLines();
-
-		if (!isFallingFinished) 
-			newPiece();
-	}
-
-	public void newPiece() {
-		currentPiece.setRandomShape();
-		currentX = BOARD_WIDTH / 2 + 1;
-		currentY = BOARD_HEIGHT - 1 + currentPiece.minY();
-
-		if (!tryMove(currentPiece, currentX, currentY -1)) {
-			currentPiece.setShape(Tetronimos.NO_SHAPE);
-			timer.stop();
-			isStarted = false;
-			statusBar.setText("Game Over");
-		}
-	}
-
-	private void drawSquare(Graphics g, int x, int y, Tetronimos shape) {
-		Color color = COLORS[shape.ordinal()];
-		g.setColor(color);
-		g.fillRect(x + 1, y + 1, squareWidth() - 2, squareHeight() - 2);
-		g.setColor(color.brighter());
-		g.drawLine(x, y + squareHeight() - 1, x, y);
-		g.drawLine(x, y, x + squareWidth() - 1, y);
-		g.setColor(color.darker());
-		g.drawLine(x + 1, y + squareHeight() - 1, x + squareWidth() - 1, y + squareHeight() - 1);
-		g.drawLine(x + squareWidth() - 1, y + squareHeight() - 1, x + squareWidth() - 1, y + 1);
-
-	}
-
-	@Override
-	public void paint(Graphics g) {
-		super.paint(g);
-		Dimension size = getSize();
-
-		int boardTop = (int) size.getHeight() - BOARD_HEIGHT * squareHeight();
-
-		for (int i = 0; i < BOARD_HEIGHT; i++) {
-			for (int j = 0; j < BOARD_WIDTH; ++j) {
-				Tetronimos shape = shapeAt(j, BOARD_HEIGHT - i - 1);
-
-				if (shape != Tetronimos.NO_SHAPE) {
-					drawSquare(g, j * squareWidth(), boardTop + i * squareHeight(), shape);
-
-				}
-			}
-		}
-
-		if (currentPiece.getShape() != Tetronimos.NO_SHAPE) {
-			
-			for (int i = 0; i < currentPiece.getX(); ++i) {
-				int x = currentX + currentPiece.x(i);
-				int y = currentY + currentPiece.y(i);
-				drawSquare(g, x * squareWidth(), boardTop + (BOARD_HEIGHT - y - 1) * squareHeight(),
-						currentPiece.getShape());
-			}
-		}
-
-	}
-	
-	public void start(){
-		if(isPaused)
-			return;
-		
-		isStarted = true;
-		isFallingFinished = false;
-		numLinesRemoved = 0;
-		clearBoard();
-		newPiece();
-		timer.start();
-		
-	}
-	
-	public void pause(){
-		if(!isStarted)
-			return;
-		
-		isPaused = !isPaused;
-		
-		if(isPaused) {
-			timer.stop();
-			statusBar.setText("Paused");
-			
-		}
-		else{
-			timer.start();
-			statusBar.setText(String.valueOf(numLinesRemoved));
-			
-		}
-		
-		repaint();
-	}
-	
-	private boolean tryMove(Shape newPiece, int newX,int newY){
-		for (int i =0;i < newPiece.getX(); ++i){
-			int x = newX + newPiece.x(i);
-			int y = newY - newPiece.y(i);
-			
-			if(x < 0 || x >= BOARD_WIDTH || y < 0 || y >= BOARD_HEIGHT)
-				return false;
-			
-			if(shapeAt(x, y) != Tetronimos.NO_SHAPE)
-				return false;	
-		}
-		
-		currentPiece = newPiece;
-		currentX = newX;
-		currentY = newY;
-		repaint();
-		
-		return true;
-	}
-	
-	private void removeFullLines(){
-		int numFullLines = 0;
-		
-		for (int i = BOARD_HEIGHT - 1; i >= 0; i--){
-			boolean lineIsFull = true;
-			
-			for (int j =0; j < BOARD_WIDTH ; j++){
-				if (shapeAt(j, i) == Tetronimos.NO_SHAPE) {
-					lineIsFull = false;
-					break;
-					
-				}
-			}
-			
-			if (lineIsFull) {
-				numFullLines++;
-				
-				for(int k = i; k  < BOARD_HEIGHT - 1;k++){
-					for( int j = 0; j < BOARD_WIDTH; j++)
-						board [ k * BOARD_WIDTH + j] = shapeAt(j, k + 1);
-				}
-			}
-			
-			if (numFullLines > 0) {
-				numLinesRemoved += numFullLines;
-				statusBar.setText(String.valueOf(numLinesRemoved));
-				isFallingFinished = true;
-				currentPiece.setShape(Tetronimos.NO_SHAPE);
-				repaint();
-			}
-		}
-	}
-	
-	
-	private void dropDown() {
-		int newY = currentY;
-		
-		while (newY > 0){
-			
-			if(!tryMove (currentPiece,currentX,newY -1))
-				break;
-			
-			--newY;
-		}
-		
-		pieceDrop();
-	}
-	
-	private void oneLineDown(){
-		if (!tryMove(currentPiece, currentX, currentY - 1))
-			pieceDrop();
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		if (isFallingFinished) {
-		      isFallingFinished = false;
-		      newPiece();
-		    } else {
-		      oneLineDown();
-		    }
-	}
+	    setFocusable(true);
+	    currentPiece = new Shape();
+	    timer = new Timer(TIME, this); // timer for lines down
+	    statusBar = parent.getStatusBar();
+	    board = new Tetronimos[BOARD_WIDTH * BOARD_HEIGHT];
+	    clearBoard();
+	    addKeyListener(new TetrisAdapter());
+	  }
+	 
+	  public int squareWidth() {
+	    return (int) getSize().getWidth() / BOARD_WIDTH;
+	  }
+	 
+	  public int squareHeight() {
+	    return (int) getSize().getHeight() / BOARD_HEIGHT;
+	  }
+	 
+	  public Tetronimos shapeAt(int x, int y) {
+	    return board[y * BOARD_WIDTH + x];
+	  }
+	 
+	  private void clearBoard() {
+	    for (int i = 0; i < BOARD_HEIGHT * BOARD_WIDTH; i++) {
+	      board[i] = Tetronimos.NO_SHAPE;
+	    }
+	  }
+	 
+	  private void pieceDropped() {
+	    for (int i = 0; i < currentPiece.getX(); i++) {
+	      int x = currentX + currentPiece.x(i);
+	      int y = currentY - currentPiece.y(i);
+	      board[y * BOARD_WIDTH + x] = currentPiece.getShape();
+	    }
+	 
+	    removeFullLines();
+	 
+	    if (!isFallingFinished) {
+	      newPiece();
+	    }
+	  }
+	 
+	  public void newPiece() {
+	    currentPiece.setRandomShape();
+	    currentX = BOARD_WIDTH / 2 + 1;
+	    currentY = BOARD_HEIGHT - 1 + currentPiece.minY();
+	 
+	    if (!tryMove(currentPiece, currentX, currentY - 1)) {
+	      currentPiece.setShape(Tetronimos.NO_SHAPE);
+	      timer.stop();
+	      isStarted = false;
+	      statusBar.setText("Game Over");
+	    }
+	  }
+	 
+	  private void oneLineDown() {
+	    if (!tryMove(currentPiece, currentX, currentY - 1))
+	      pieceDropped();
+	  }
+	 
+	  @Override
+	  public void actionPerformed(ActionEvent ae) {
+	    if (isFallingFinished) {
+	      isFallingFinished = false;
+	      newPiece();
+	    } else {
+	      oneLineDown();
+	    }
+	  } 
+	 
+	  private void drawSquare(Graphics g, int x, int y, Tetronimos shape) {
+		  Color color = COLORS[shape.ordinal()];
+	    g.setColor(color);
+	    g.fillRect(x + 1, y + 1, squareWidth() - 2, squareHeight() - 2);
+	    g.setColor(color.brighter());
+	    g.drawLine(x, y + squareHeight() - 1, x, y);
+	    g.drawLine(x, y, x + squareWidth() - 1, y);
+	    g.setColor(color.darker());
+	    g.drawLine(x + 1, y + squareHeight() - 1, x + squareWidth() - 1, y + squareHeight() - 1);
+	    g.drawLine(x + squareWidth() - 1, y + squareHeight() - 1, x + squareWidth() - 1, y + 1);
+	  }
+	 
+	  @Override
+	  public void paint(Graphics g) {
+	    super.paint(g);
+	    Dimension size = getSize();
+	    int boardTop = (int) size.getHeight() - BOARD_HEIGHT * squareHeight();
+	 
+	    for (int i = 0; i < BOARD_HEIGHT; i++) {
+	      for (int j = 0; j < BOARD_WIDTH; ++j) {
+	        Tetronimos shape = shapeAt(j, BOARD_HEIGHT - i - 1);
+	 
+	        if (shape != Tetronimos.NO_SHAPE) {
+	          drawSquare(g, j * squareWidth(), boardTop + i * squareHeight(), shape);
+	        }
+	      }
+	    }
+	 
+	    if (currentPiece.getShape() != Tetronimos.NO_SHAPE) {
+	      for (int i = 0; i < currentPiece.getX(); ++i) {
+	        int x = currentX + currentPiece.x(i);
+	        int y = currentY - currentPiece.y(i);
+	        drawSquare(g, x * squareWidth(), boardTop + (BOARD_HEIGHT - y - 1) * squareHeight(), currentPiece.getShape());
+	      }
+	    }
+	  }
+	 
+	  public void start() {
+	    if (isPaused)
+	      return;
+	 
+	    isStarted = true;
+	    isFallingFinished = false;
+	    numLinesRemoved = 0;
+	    clearBoard();
+	    newPiece();
+	    timer.start();
+	  }
+	 
+	  public void pause() {
+	    if (!isStarted)
+	      return;
+	 
+	    isPaused = !isPaused;
+	 
+	    if (isPaused) {
+	      timer.stop();
+	      statusBar.setText("Paused");
+	    } else {
+	      timer.start();
+	      statusBar.setText(String.valueOf(numLinesRemoved));
+	    }
+	 
+	    repaint();
+	  }
+	 
+	  private boolean tryMove(Shape newPiece, int newX, int newY) {
+	    for (int i = 0; i < currentPiece.getX(); ++i) {
+	      int x = newX + newPiece.x(i);
+	      int y = newY - newPiece.y(i);
+	 
+	      if (x < 0 || x >= BOARD_WIDTH || y < 0 || y >= BOARD_HEIGHT)
+	        return false;
+	 
+	      if (shapeAt(x, y) != Tetronimos.NO_SHAPE)
+	        return false;
+	    }
+	 
+	    currentPiece = newPiece;
+	    currentX = newX;
+	    currentY = newY;
+	    repaint();
+	 
+	    return true;
+	  }
+	 
+	  private void removeFullLines() {
+	    int numFullLines = 0;
+	 
+	    for (int i = BOARD_HEIGHT - 1; i >= 0; --i) {
+	      boolean lineIsFull = true;
+	 
+	      for (int j = 0; j < BOARD_WIDTH; ++j) {
+	        if (shapeAt(j, i) == Tetronimos.NO_SHAPE) {
+	          lineIsFull = false;
+	          break;
+	        }
+	      }
+	 
+	      if (lineIsFull) {
+	        ++numFullLines;
+	 
+	        for (int k = i; k < BOARD_HEIGHT - 1; ++k) {
+	          for (int j = 0; j < BOARD_WIDTH; ++j) {
+	            board[k * BOARD_WIDTH + j] = shapeAt(j, k + 1);
+	          }
+	        }
+	      }
+	 
+	      if (numFullLines > 0) {
+	        numLinesRemoved += numFullLines;
+	        statusBar.setText(String.valueOf(numLinesRemoved));
+	        isFallingFinished = true;
+	        currentPiece.setShape(Tetronimos.NO_SHAPE);
+	        repaint();
+	      }
+	    }
+	  }
+	 
+	  private void dropDown() {
+	    int newY = currentY;
+	 
+	    while (newY > 0) {
+	      if (!tryMove(currentPiece, currentX, newY - 1))
+	        break;
+	 
+	      --newY;
+	    }
+	 
+	    pieceDropped();
+	  }
 	
 	class TetrisAdapter extends KeyAdapter {
 		@Override
--- Tetris/src/com/nnc/tetris/Shape.java
+++ Tetris/src/com/nnc/tetris/Shape.java
@@ -1,135 +1,132 @@
 package com.nnc.tetris;
+
 import java.util.Random;
 
 public class Shape {
-	
+
 	enum Tetronimos {
-		NO_SHAPE (new int[][] {{0,0}, {0,0},{0,0},{0,0}}),
-		Z_SHAPE ((new int[][] {{0,-1}, {0,0},{-1,0},{-1,1}})),
-		S_SHAPE ((new int[][] {{0,-1}, {0,0},{1,0},{1,1}})),
-		LINE_SHAPE ((new int[][] {{0,-1}, {0,0},{0,1},{0,2}})),
-		T_SHAPE ((new int[][] {{-1,0}, {0,0},{1,0},{0,1}})),
-		SQUARE_SHAPE ((new int[][] {{0,0}, {1,0},{0,1},{1,1}})),
-		L_SHAPE ((new int[][] {{-1,-1}, {0,-1},{0,0},{0,1}})),
-		MIRROR_L_SHAPE ((new int[][] {{1,-1}, {0,-1},{0,0},{0,1}}))
-		;
-		
+		NO_SHAPE(new int[][] { { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 } }), Z_SHAPE(
+				(new int[][] { { 0, -1 }, { 0, 0 }, { -1, 0 }, { -1, 1 } })), S_SHAPE(
+						(new int[][] { { 0, -1 }, { 0, 0 }, { 1, 0 }, { 1, 1 } })), LINE_SHAPE(
+								(new int[][] { { 0, -1 }, { 0, 0 }, { 0, 1 }, { 0, 2 } })), T_SHAPE(
+										(new int[][] { { -1, 0 }, { 0, 0 }, { 1, 0 }, { 0, 1 } })), SQUARE_SHAPE(
+												(new int[][] { { 0, 0 }, { 1, 0 }, { 0, 1 }, { 1, 1 } })), L_SHAPE(
+														(new int[][] { { -1, -1 }, { 0, -1 }, { 0, 0 },
+																{ 0, 1 } })), MIRROR_L_SHAPE(
+																		(new int[][] { { 1, -1 }, { 0, -1 }, { 0, 0 },
+																				{ 0, 1 } }));
+
 		public int[][] coords;
-		
-		private Tetronimos (int[][] coords){
+
+		private Tetronimos(int[][] coords) {
 			this.coords = coords;
 		}
 	}
-	
+
 	private final int X = 4;
 	private final int Y = 2;
 	private Tetronimos pieceShape;
 	private int[][] coords;
-	
-	
-	
-	public Shape(){
+
+	public Shape() {
 		coords = new int[X][Y];
 		setShape(Tetronimos.NO_SHAPE);
 	}
 
-
 	public void setShape(Tetronimos shape) {
-		for(int i=0;i<X;i++)
-			for(int j=0;j<Y;j++)
+		for (int i = 0; i < X; i++)
+			for (int j = 0; j < Y; j++)
 				coords[i][j] = shape.coords[i][j];
-		
+
 		pieceShape = shape;
-		
+
 	}
-	
-	private void setX(int index, int x){
+
+	private void setX(int index, int x) {
 		coords[index][0] = x;
 	}
-	
-	private void setY(int index, int y){
+
+	private void setY(int index, int y) {
 		coords[index][1] = y;
 	}
-	
-	public int x(int index){
+
+	public int x(int index) {
 		return coords[index][0];
 	}
-	
-	public int y(int index){
+
+	public int y(int index) {
 		return coords[index][1];
 	}
-	
-	public Tetronimos getShape(){
+
+	public Tetronimos getShape() {
 		return pieceShape;
 	}
-	
-	public void setRandomShape(){
+
+	public void setRandomShape() {
 		Random r = new Random();
 		int x = Math.abs(r.nextInt()) % 7 + 1;
 		Tetronimos[] values = Tetronimos.values();
 		setShape(values[x]);
-		
+
 	}
-	
-	public int minX(){
+
+	public int minX() {
 		int min = coords[0][0];
-		
-		for (int i =0; i < X; i++){
+
+		for (int i = 0; i < X; i++) {
 			min = Math.min(min, coords[i][0]);
 		}
-		
+
 		return min;
-		
+
 	}
-	
-	public int minY(){
+
+	public int minY() {
 		int min = coords[0][1];
-		
-		for (int i = 0; i < Y; i++){
+
+		for (int i = 0; i < Y; i++) {
 			min = Math.min(min, coords[i][1]);
 		}
-		
+
 		return min;
-		
+
 	}
-	
-	public Shape rotateLeft (){
-		if(pieceShape == Tetronimos.SQUARE_SHAPE)
+
+	public Shape rotateLeft() {
+		if (pieceShape == Tetronimos.SQUARE_SHAPE)
 			return this;
-		
+
 		Shape result = new Shape();
 		result.pieceShape = pieceShape;
-		
-		for(int i=0; i < X; i++){
-			result.setX(i,  y(i));
+
+		for (int i = 0; i < X; i++) {
+			result.setX(i, y(i));
 			result.setY(i, -x(i));
 		}
-		
+
 		return result;
 	}
-	
-	public Shape rotateRight (){
-		if(pieceShape == Tetronimos.SQUARE_SHAPE)
+
+	public Shape rotateRight() {
+		if (pieceShape == Tetronimos.SQUARE_SHAPE)
 			return this;
-		
+
 		Shape result = new Shape();
 		result.pieceShape = pieceShape;
-		
-		for(int i=0; i < X; i++){
-			result.setX(i,  -y(i));
+
+		for (int i = 0; i < X; i++) {
+			result.setX(i, -y(i));
 			result.setY(i, x(i));
 		}
-		
+
 		return result;
 	}
-	
-	
-	
-	public int getX(){
+
+	public int getX() {
 		return X;
 	}
 
-	public int getY(){
+	public int getY() {
 		return Y;
 	}
 }
